ab1a4c93af06cabd9054d1113aecc4de80dfef16
Sebastian Delima <sebastiandelimagec@gmail.com>
2020-05-22 22:03:39 -0500

    almost done

--- index.cob
+++ index.cob
@@ -1,21 +1,41 @@
+        
+        *> FIRST DIVISION
         IDENTIFICATION DIVISION.
         
         PROGRAM-ID. BLOGPOST.
         AUTHOR.     SEBASTIAN DE LIMA.
 
+        *> SECOND DIVISION   
         ENVIRONMENT DIVISION.
+        
+        *> FIRST SECTION INSIDE THE ENVIRONMENT DIVISION
+        CONFIGURATION SECTION.
+        Source-Computer. MACBOOK PRO.
+        Object-Computer. MACBOOK PRO.
 
+        *> SECOND SECTION INSIDE THE ENVIRONMENT DIVISION
+        INPUT-OUTPUT SECTION.
+
+
+        *> THIRD DIVISION
         DATA DIVISION.
 
-        FILE SECTION.
         WORKING-STORAGE SECTION.
-        01 LoverName PIC X(10).
+        01 MyName          PIC A(20) VALUE "Sebastian De Lima". 
+        01 FavoriteNumber  PIC 9 VALUE 7.
+        01 Age             PIC X(20) VALUE "24 Years old".
+        01 DateBirth.
+           02 Month PIC 9(2).
+           02 Dayy   PIC 9(2).
+           02 Year  PIC 9(4).
 
+        *> FOURTH DIVISION
         PROCEDURE DIVISION.
 
-        DISPLAY "Put your lovers name"
-        ACCEPT LoverName
-        DISPLAY "I love " LoverName
+        COMPUTE 1 + 2
 
         STOP RUN.
-        
+
+
+      
+      
